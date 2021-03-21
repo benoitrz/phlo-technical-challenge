2d2ccd52e0967a27226c47f7b30fc90db4798134
Benoit Trzpit <benoit.trz@gmail.com>
2021-03-21 15:49:40 +0000

    Add base map using leaflet and react-leaflet bindings

--- src/components/FindDoctor.js
+++ src/components/FindDoctor.js
@@ -1,5 +1,22 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
+
 const FindDoctor = () => {
-  return <div>Find a doctor</div>;
+  const height = "400px";
+  const zoomLevel = 15;
+  const [mapCenter, setMapCenter] = useState([51.5074, 0.1278]); // London coordinates by default
+
+  return (
+    <div>
+      <MapContainer center={mapCenter} zoom={zoomLevel} style={{ height }}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default FindDoctor;
